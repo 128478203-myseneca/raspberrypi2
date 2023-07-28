832fb7a34441829e6d065c14b2a62589efd00464
Istvan Feher <62614194+128478203-myseneca@users.noreply.github.com>
2023-07-28 13:11:50 -0400

    first assigment2 commit

--- iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
+++ iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
@@ -175,13 +175,11 @@ def sendtransactiondata(maintopic,mainproducerid,VIPERPORT,index,preprocesstopic
  
 #	  // check for payload  'uid=subject.reference,filter:resourceType=MedicationAdministration,payload=payload.payload~\
 
-     jsoncriteria='uid=metadata.dsn,filter:allrecords~\
-subtopics=metadata.property_name~\
-values=datapoint.value~\
-identifiers=metadata.display_name~\
-datetime=datapoint.updated_at~\
-msgid=datapoint.id~\
-latlong=lat:long'     
+     jsoncriteria = 'uid=id~\
+subtopics=Datetime,DAYTON_MW~\
+values=DAYTON_MW~\
+identifiers=id~\
+datetime=Datetime' 
 
 #     jsoncriteria='uid=entry.0.resource.id,filter:allrecords~\
 #subtopics=entry.1.resource.type.0.coding.0.code~\
